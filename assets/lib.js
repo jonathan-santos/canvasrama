d6d40b1cf536500a4f409900055aa7efe4168c90
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-12-19 13:43:58 -0300

    Adicionado funções drawLine e drawOval na lib e adicionado o uso do clearRect

--- assets/lib.js
+++ assets/lib.js
@@ -1,7 +1,25 @@
 const canvas = document.querySelector('#game')
 const ctx = canvas.getContext('2d')
 
+
 const initGame = (game, fps = 1000 / 30) => {
     const loop = setInterval(game, fpsp)
     return loop
+}
+
+ctx.drawOval = (x, y, size, fill = true) => {
+    ctx.beginPath()
+    ctx.arc(x, y, size, 0, Math.PI * 2, false)
+
+    if(fill)
+        ctx.fill()
+    else
+        ctx.stroke()
+}
+
+ctx.drawLine = (x, y, endX, endY) => {
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+    ctx.lineTo(endX, endY)
+    ctx.stroke()
 }
--- cap1_prototipoDeJogo/desafio1.js
+++ cap1_prototipoDeJogo/desafio1.js
@@ -1,28 +1,19 @@
-ctx.fillStyle = '#fff'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 // Quadrado não preenchido
 ctx.strokeStyle = '#000'
 ctx.strokeRect(20, 50, 30, 30)
 
 // Círculo não preenchido
-ctx.beginPath()
-ctx.arc(80, 65, 30, 0, Math.PI * 2, true)
-ctx.stroke()
+ctx.drawOval(80, 65, 30, false)
 
 // Linha tracejada vertical
 ctx.setLineDash([4, 2])
-ctx.beginPath()
-ctx.moveTo(canvas.width / 2, 0)    
-ctx.lineTo(canvas.width / 2, canvas.height)
-ctx.stroke()
+ctx.drawLine(canvas.width / 2, 0, canvas.width / 2, canvas.height)
 
 // Linha normal horizontal
 ctx.setLineDash([0])
-ctx.beginPath()
-ctx.moveTo(0, canvas.height / 2)
-ctx.lineTo(canvas.width, canvas.height / 2)
-ctx.stroke()
+ctx.drawLine(0, canvas.height / 2, canvas.width, canvas.height / 2)
 
 ctx.fillStyle = '#ff0'
 
@@ -30,9 +21,7 @@ ctx.fillStyle = '#ff0'
 ctx.fillRect(250, 180, 200, 200)
 
 // Círculo preenchido
-ctx.beginPath()
-ctx.arc(700, 520, 200, 0, Math.PI * 2, true)
-ctx.fill()
+ctx.drawOval(700, 520, 200)
 
 // Texto
 ctx.fillStyle = '#f00'
